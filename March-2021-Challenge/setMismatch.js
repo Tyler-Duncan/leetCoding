5a9e4998d60339f0f0545424d1f1491809e9e4ac
Tyler Duncan <tylerduncan365@yahoo.com>
2021-03-02 15:20:25 -0600

    refactor: Updated pseudocode Updated the pseudocode to be more precise and explanative

--- March-2021-Challenge/setMismatch.js
+++ March-2021-Challenge/setMismatch.js
@@ -2,46 +2,54 @@
  * @param {number[]} nums
  * @return {number[]}
  */
-var findErrorNums = function(nums) {
+var findErrorNums = function (nums) {
   //We are looking for the value that appears twice and the value that is not present in the array
-  //Knowing this we can go through the array and add the duplicated value to the the answer we'll
-  //be returning
 
-  //And to find the value missing we can create a reference array and find what value is present
-  //in it and not present in the input array
+  //Knowing this we can create a counter object to track the number of times a
+  //value appears in the input array
 
-  //establish return array and counter
+  //We can then go through this counter to find the key that has a value of 2 and
+  //parse it to be a number (since objects use strings for keys)
+
+  //To find the missing value we can create a (correct) reference array and find what
+  //value is present in it and not present in the input array
+
+  //establish return array, reference array and counter object
   let dupeAndMissing = [];
   let reference = [];
   let counter = {};
 
   //populate reference array and counter
   for (let i = 0; i < nums.length; i++) {
-      //add 1 -> nums.length to reference array
-      reference.push(i+1);
+    //add 1 -> nums.length to reference array
+    reference.push(i + 1);
 
-      //count number of times a value appears
-      if (counter[nums[i]]) {
-          counter[nums[i]]++
-      } else {
-          counter[nums[i]] = 1;
-      }
+    //count number of times a value appears
+    //if the counter already has the value increment
+    if (counter[nums[i]]) {
+      counter[nums[i]]++
+      //else create the key and set it's value to one
+    } else {
+      counter[nums[i]] = 1;
+    }
   }
 
-  //iterate through the object and find the value that occured twice
+  //iterate through the counter object and find the value that occured twice
   for (key in counter) {
-      if (counter[key] === 2) {
-          dupeAndMissing.push(parseInt(key));
-      }
+    if (counter[key] === 2) {
+      dupeAndMissing.push(parseInt(key));
+    }
   }
 
   //iterate through the reference (correct) array
   for (let j = 0; j < reference.length; j++) {
-  //push the value that is missing from the input array
-      if (!nums.includes(reference[j])) {
-          dupeAndMissing.push(reference[j]);
-      }
+    //if the input array does not have the current value in the correct array
+    if (!nums.includes(reference[j])) {
+      //push that value, since it is missing
+      dupeAndMissing.push(reference[j]);
+    }
   }
-  //return the answer array
+
+  //return the array housing the duplicate and the missing values
   return dupeAndMissing;
 };
