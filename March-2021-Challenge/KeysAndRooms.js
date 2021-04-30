15d658e9d5c75f79d3951e18e5bed8985b6d4b47
Tyler Duncan <tylerduncan365@yahoo.com>
2021-04-30 12:30:03 -0500

    update: uploading since hiatus

--- March-2021-Challenge/KeysAndRooms.js
+++ March-2021-Challenge/KeysAndRooms.js
@@ -31,7 +31,70 @@
 /**
  * @param {number[][]} rooms
  * @return {boolean}
+ *
+ * I: An array containing arrays with numbers corresponding to the keys to other rooms
+ *
+ * O: Boolean
+ *
+ * C:
+ *
+ * E:Starting room with 0 keys
+ *
+ * HL: Going through rooms and gathering keys, check if all rooms can be entered. Going back into rooms is allowed.
+ * Idea:
+ * Go through each room with a for loop and add the keys in the room to a hash table
+ * if we have the key to the next room then continue the iteration, if not then return false
+ *
+ *
  */
  var canVisitAllRooms = function(rooms) {
+//establish keys available
+const keys = [];
+  //start with room 0 begin iteration
+  for (let i = 0; i < rooms.length; i++) {
+    //add the current rooms keys to our keys object
+    for (let j = 0; j < rooms[i].length; j++) {
+      if (!keys.includes(rooms[i][j])) {
+        keys.push(rooms[i][j]);
+      }
+    }
 
+
+    //if key for next room is not available and there are no more rooms return false
+    if (i < rooms.length - 1 && !keys.includes(i + 1)) {
+      console.log(keys)
+      return false;
+    }
+    //otherwise continue iteration
+
+  }
+//return true
+return true;
 };
+
+console.log(canVisitAllRooms([[1],[2],[3],[]]));
+
+console.log(canVisitAllRooms([[1,3],[3,0,1],[2],[0]]));
+
+
+
+var canVisitAllRooms = function(rooms) {
+  //establish keys available
+  let keys = {}
+  let i = 0;
+  let j = rooms.length - 1;
+    while (i <= j) {
+    keys =	getKeys(rooms[i], keys);
+    keys = 	getKeys(rooms[j], keys);
+      if (!hasRightKey(i, keys) && !hasLeftKey(j, keys)) return false;
+      if (hasRightKey(i, keys)) i++;
+      if (hasLeftKey(j, keys)) j--;
+    }
+  return true;
+  }
+  const getKeys = (arr, keys) =>{
+     arr.forEach(key => keys[key] = true);
+     return keys
+     };
+  const hasLeftKey = (idx, keys) => keys[(idx - 1).toString()];
+  const hasRightKey = (idx, keys) => keys[(idx + 1).toString()];
